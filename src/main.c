832eaf194e1fe56b0dd6af27f37f7b521c154834
Ikey Doherty <ikey@solus-project.com>
2018-04-30 09:49:39 +0100

    Create a BudgieDesktopManager during app cycle
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/main.c
+++ src/main.c
@@ -11,11 +11,30 @@
 
 #include <gtk/gtk.h>
 
+#include "desktop-manager.h"
+#include "util.h"
+
 #define APP_ID "org.budgie-desktop.icons"
 
-static void app_activate(GApplication *app, gpointer v)
+static BudgieDesktopManager *manager = NULL;
+
+static void app_activate(__budgie_unused__ GApplication *app, __budgie_unused__ gpointer v)
 {
+        if (manager) {
+                return;
+        }
+        manager = budgie_desktop_manager_new();
         g_message("Activated..");
+}
+
+static void app_shutdown(__budgie_unused__ GApplication *app, __budgie_unused__ gpointer v)
+{
+        if (!manager) {
+                return;
+        }
+        g_object_unref(manager);
+        manager = NULL;
+        g_message("Shutdown");
 }
 
 int main(int argc, char **argv)
@@ -24,6 +43,7 @@ int main(int argc, char **argv)
 
         app = gtk_application_new(APP_ID, G_APPLICATION_FLAGS_NONE);
         g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+        g_signal_connect(app, "shutdown", G_CALLBACK(app_shutdown), NULL);
         return g_application_run(G_APPLICATION(app), argc, argv);
 }
 
